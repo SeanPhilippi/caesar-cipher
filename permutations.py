85e11da8f10952cf99794324306555d188376adc
SeanPhilippi <sean.philippi@gmail.com>
2021-01-08 17:51:29 -0600

    using cProfile to time permutations

--- permutations.py
+++ permutations.py
@@ -1,11 +1,15 @@
-from itertools import permutations
+import cProfile
 
-my_list = [1, 2, 3, 4]
+def faculty(n):
+	if n <= 1:
+		return n
+	else:
+		return faculty(n - 1) * n
+		
+def counter(n):
+	cnt = 0
+	for i in range(n):
+		cnt += 1
+	return cnt
 
-list_of_permutations = permutations(my_list)
-cnt = 0
-for permutation in list_of_permutations:
-	cnt += 1
-	print(permutation)
-	
-print(len(my_list), cnt)
+cProfile.run('counter(faculty(12))')
