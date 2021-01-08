dca3e990f6d26e30e807060b563559b6bdc9db79
SeanPhilippi <sean.philippi@gmail.com>
2021-01-08 17:38:02 -0600

    creating list of permutations

---
+++ permutations.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+
+my_list = [1, 2, 3, 4]
+
+list_of_permutations = permutations(my_list)
+cnt = 0
+for permutation in list_of_permutations:
+	cnt += 1
+	print(permutation)
+	
+print(len(my_list), cnt)
