edeb1f857aac9d4b20289a4b566fd0cd4eff0f84
SeanPhilippi <sean.philippi@gmail.com>
2021-01-09 10:31:42 -0600

    decrypting message

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -17,9 +17,19 @@ def encrypt(key, message):
 			cipher += c
 	return cipher
 
+def get_decrypt_key(key):
+	dkey = {}
+	# swap key and values in key to create dkey
+	for k in key:
+		dkey[key[k]] = k
+	return dkey
 
 key = generate_key()
 print(key)
 message = "YOU ARE AWESOME"
 cipher = encrypt(key, message)
 print(cipher)
+
+dkey = get_decrypt_key(key)
+message = encrypt(dkey, cipher)
+print('message', message)
